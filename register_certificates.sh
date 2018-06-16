0e44e8b134ddfb3419c5d13eedf0ec5f535f8400
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-06-16 12:02:13 +0200

    Configure grafana.nur-jan.de

--- register_certificates.sh
+++ register_certificates.sh
@@ -2,4 +2,5 @@ letsencrypt certonly --expand --webroot \
   -w /var/www/letsencrypt/nur-jan.de/ -d nur-jan.de \
   -w /var/www/letsencrypt/sprit.nur-jan.de/ -d sprit.nur-jan.de \
   -w /var/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de \
-  -w /var/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de
+  -w /var/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de \
+  -w /var/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de
