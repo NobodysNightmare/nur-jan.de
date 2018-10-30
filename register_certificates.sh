bc85b192f788e2da5a41332e7db02b16331b625f
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-10-30 17:16:18 +0100

    Update configurationfor expenses app

--- register_certificates.sh
+++ register_certificates.sh
@@ -4,3 +4,4 @@ letsencrypt certonly --expand --webroot \
   -w /var/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de \
   -w /var/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de \
   -w /var/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de
+  -w /var/www/letsencrypt/expenses.nur-jan.de/ -d expenses.nur-jan.de
