221c8fed7a1b352bfbc91968623093879d546b5d
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-11-05 20:30:53 +0100

    Only overwrite databases.sql.gz when dumping finished
    Otherwise requesting the file while the backup is in progress
    can lead to fetching an incomplete dump.

--- dump_databases.sh
+++ dump_databases.sh
@@ -1,1 +1,3 @@
-mysqldump --databases dynamic_dns energy expenses grafana sprit | gzip > /var/www/nur-jan.de/backups/databases.sql.gz
+set -e
+mysqldump --databases dynamic_dns energy expenses grafana sprit | gzip > /var/www/nur-jan.de/backups/databases-in-progress.sql.gz
+mv -f /var/www/nur-jan.de/backups/databases-in-progress.sql.gz /var/www/nur-jan.de/backups/databases.sql.gz
