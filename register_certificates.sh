525d4e8963f402ae563ecb56c787b4f2c770f514
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-01-07 07:46:35 +0100

    Switch to many single-domain certificates
    Making the certificates independent from each other,
    allowing to change hosting machine on a domain-by-domain
    basis.

--- register_certificates.sh
+++ register_certificates.sh
@@ -1,8 +1,7 @@
-letsencrypt certonly --expand --webroot \
-  -w /var/www/letsencrypt/nur-jan.de/ -d nur-jan.de \
-  -w /var/www/letsencrypt/sprit.nur-jan.de/ -d sprit.nur-jan.de \
-  -w /var/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de \
-  -w /var/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de \
-  -w /var/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de \
-  -w /var/www/letsencrypt/expenses.nur-jan.de/ -d expenses.nur-jan.de \
-  -w /var/www/letsencrypt/lan.nur-jan.de/ -d lan.nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/nur-jan.de/ -d nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/sprit.nur-jan.de/ -d sprit.nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/expenses.nur-jan.de/ -d expenses.nur-jan.de
+letsencrypt certonly --webroot -w /var/www/letsencrypt/lan.nur-jan.de/ -d lan.nur-jan.de
