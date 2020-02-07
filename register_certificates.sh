72f0f3d167d2722d3ae14e4ccb0943a449495ad6
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-02-07 20:41:25 +0100

    Fix cert registration scripts

--- register_certificates.sh
+++ register_certificates.sh
@@ -1,7 +1,7 @@
-letsencrypt certonly --webroot -w /var/www/letsencrypt/nur-jan.de/ -d nur-jan.de
-letsencrypt certonly --webroot -w /var/www/letsencrypt/sprit.nur-jan.de/ -d sprit.nur-jan.de
-letsencrypt certonly --webroot -w /var/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de
-letsencrypt certonly --webroot -w /var/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de
-letsencrypt certonly --webroot -w /var/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de
-letsencrypt certonly --webroot -w /var/www/letsencrypt/expenses.nur-jan.de/ -d expenses.nur-jan.de
-letsencrypt certonly --webroot -w /var/www/letsencrypt/lan.nur-jan.de/ -d lan.nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/nur-jan.de/ -d nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/sprit.nur-jan.de/ -d sprit.nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/expenses.nur-jan.de/ -d expenses.nur-jan.de
+certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/lan.nur-jan.de/ -d lan.nur-jan.de
