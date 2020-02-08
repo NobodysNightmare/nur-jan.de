c507bbefbd082e8dc7a4c493bcb75d70b2458c70
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-02-08 16:54:55 +0000

    Fix backup script to work in cron
    cron does not seem to set the same path that
    my regular bash is using... and it is not using bash
    by default either.

--- docker-compose/mariadb/dump_backups.sh
+++ docker-compose/mariadb/dump_backups.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 BACKUP_DIR=$(dirname "${BASH_SOURCE[0]}")/../../www/nur-jan.de/backups/
 
 function dump_database {
   echo "Dumping $1..."
 
-  docker exec -e DUMP_DB=$1 mariadb_db_1 sh -c 'exec mysqldump --databases $DUMP_DB -uroot -p"$MYSQL_ROOT_PASSWORD"' | gzip > $BACKUP_DIR/in_progress.sql.gz
+  /snap/bin/docker exec -e DUMP_DB=$1 mariadb_db_1 sh -c 'exec mysqldump --databases $DUMP_DB -uroot -p"$MYSQL_ROOT_PASSWORD"' | gzip > $BACKUP_DIR/in_progress.sql.gz
   mv $BACKUP_DIR/in_progress.sql.gz $BACKUP_DIR/$1.sql.gz
 }
 
