cfa08c06a36655801763be0d964273d399039b28
Jan Sandbrink <nobodysnightmare@gmail.com>
2020-05-08 13:34:09 +0200

    Remove unneeded configuration

--- register_certificates.sh
+++ register_certificates.sh
@@ -1,7 +1,6 @@
 certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/nur-jan.de/ -d nur-jan.de
 certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/sprit.nur-jan.de/ -d sprit.nur-jan.de
 certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/energy.nur-jan.de/ -d energy.nur-jan.de
-certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/dyn.nur-jan.de/ -d dyn.nur-jan.de
 certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/grafana.nur-jan.de/ -d grafana.nur-jan.de
 certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/expenses.nur-jan.de/ -d expenses.nur-jan.de
 certbot certonly --webroot -w /home/jan/config-repo/www/letsencrypt/lan.nur-jan.de/ -d lan.nur-jan.de
